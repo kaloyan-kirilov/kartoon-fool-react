ddb078177d1e1891c0e8764ce7a17fd99a06d11c
kaloyan-kirilov <kaloyan.kirilov@mail.com>
2024-06-16 21:21:36 +0300

    artwork fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/kartoon-fool-react/"
+  base: "/kartoon-fool-react/",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 });
