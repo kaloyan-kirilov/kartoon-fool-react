7f4c1034732fabe69fbe23eee221a52a5bf0b631
kaloyan-kirilov <kaloyan.kirilov@mail.com>
2024-06-17 14:28:41 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import Layout from "./components/Layout.jsx";
 import Home from "./components/pages/Home.jsx";
 import About from "./components/pages/About.jsx";
