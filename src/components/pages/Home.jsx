d2e91820eec62c964fa5a8a24796f1e551c0e665
kaloyan-kirilov <kaloyan.kirilov@mail.com>
2024-06-19 11:25:54 +0300

    update

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,10 @@
-import kartoon_fool from "../../img/kartoon-fool.png";
-
 function Home() {
     return(
         <>
             <div className="page-title">
                 <p className="title-text">A place for wacky Kartoons!</p>
             </div>
-            <img id="home-image" src={kartoon_fool} alt="kartoon fool" />
+            <div id="home-image"></div>
         </>
     );
 }
