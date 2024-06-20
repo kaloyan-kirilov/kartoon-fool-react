0cc7d2f60b0cc7314da80c26926d25ee482a6cd3
kaloyan-kirilov <kaloyan.kirilov@mail.com>
2024-06-20 12:59:16 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,19 @@ import Gallery from "./components/pages/Gallery.jsx";
 import Contact from "./components/pages/Contact.jsx";
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
 
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="/kartoon-fool-react/" element={<Layout />}>
-//       <Route index element={<Home />} />
-//       <Route path="/kartoon-fool-react/about" element={<About />} />
-//       <Route path="/kartoon-fool-react/gallery" element={<Gallery />} />
-//       <Route path="/kartoon-fool-react/contact" element={<Contact />} />
-//     </Route>
-//   )
-// );
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/kartoon-fool-react/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="/kartoon-fool-react/about" element={<About />} />
+      <Route path="/kartoon-fool-react/gallery" element={<Gallery />} />
+      <Route path="/kartoon-fool-react/contact" element={<Contact />} />
+    </Route>
+  )
+);
 
 function App() {
-  // return <RouterProvider router={router} />
-
-  return (console.log("App loaded!"))
+  return <RouterProvider router={router} />
 }
 
 export default App;
